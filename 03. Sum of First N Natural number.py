bd6d443ed3347ab19d93e755381f433c33348ca2
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-01 20:35:47 +0530

    Create 03. Sum of First N Natural number.py

---
+++ 03. Sum of First N Natural number.py
@@ -0,0 +1,9 @@
+Link : https://prepinsta.com/python-program/find-the-sum-of-first-n-natural-numbers/
+
+n=int(input("Enter a number: "))
+i=1
+total=0
+while i<=n:
+    total=total+i
+    i=i+1
+print(total)
