217231f178651b7d5aa66ce30d4a06eebc159634
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-01 17:09:54 +0530

    Create 02. Even or Odd number.py

---
+++ 02. Even or Odd number.py
@@ -0,0 +1,12 @@
+Link : https://prepinsta.com/python-program/Check-Whether-a-Number-is-Even-or-Odd/
+
+def even_odd(n):
+    if l%2==0:
+        print("Even")
+
+    else:
+        print('Odd')
+        
+n=int(input('Input a number: ') )
+
+even_odd(n)
