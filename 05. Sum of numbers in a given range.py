c3b180008d84a3d355918cc93bfbeeadd66a9382
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-01 20:54:01 +0530

    Create 05. Sum of numbers in a given range.py

---
+++ 05. Sum of numbers in a given range.py
@@ -0,0 +1,8 @@
+Link : https://prepinsta.com/python-sum-of-numbers-in-a-given-range-in-python/
+    
+num1=int(input('Enter Upper Limit no. : '))
+num2=int(input('Enter lower Limit no. : '))
+total =0
+for i in range(num1,num2+1):
+    total=total+i
+print(total)
