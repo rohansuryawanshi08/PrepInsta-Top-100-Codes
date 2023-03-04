10fc4b672d5bf5d667fa36a360a3398ced2903af
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-04 22:59:26 +0530

    Create 14.Armstrong number.py

---
+++ 14.Armstrong number.py
@@ -0,0 +1,15 @@
+Link : https://prepinsta.com/python-program/to-find-a-number-is-armstrong-or-not/
+
+Code 1 :
+num = int(input("Input a number : "))
+sum = 0
+n = num
+while n > 0:
+    digit = n % 10
+    sum += digit ** 3
+    n = n // 10
+
+if num == sum:
+    print(f"{num} is an armstrong number")
+else:
+    print(f"{num} is not an armstrong number")
