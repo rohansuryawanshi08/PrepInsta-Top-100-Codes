cf5780509ff9a6916d502afa9f32461f6fe07871
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-05 00:47:24 +0530

    Create 15. Armstrong number in a given range.py

---
+++ 15. Armstrong number in a given range.py
@@ -0,0 +1,16 @@
+Link : https://prepinsta.com/python-program/armstrong-numbers-between-two-intervals/
+
+r1 = int(input("Input upper limit : "))
+r2 = int(input("Input lower limit : "))
+
+for i in range(r1, r2):
+    n = i
+    sum = 0
+    while n > 0:
+        digit = n % 10
+        sum = sum + digit ** 3
+        n = n // 10
+
+    if sum == i:
+        print(f"{i} is Armstrong number between range")
+    
