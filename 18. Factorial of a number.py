10a9f092d7ce6123a9edfb390b94f8843afc0974
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-05 22:34:59 +0530

    Create 18. Factorial of a number.py

---
+++ 18. Factorial of a number.py
@@ -0,0 +1,10 @@
+Link : https://prepinsta.com/python-program/factorial-of-a-number/
+    
+num=int(input("Enter a number: "))
+fact=1
+a=1
+while a<=num:
+    fact=fact*a
+    a=a+1
+
+print("The factorial of ",num  , "is" , fact)
