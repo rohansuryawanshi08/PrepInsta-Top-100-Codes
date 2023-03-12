66d1a3b813372c8cbf8a49cc556bc523e305e759
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-12 10:35:10 +0530

    Create 57.Find the Smallest and largest element in an array.py

---
+++ 57.Find the Smallest and largest element in an array.py
@@ -0,0 +1,8 @@
+Link : https://prepinsta.com/python-program/find-the-smallest-and-largest-element-in-an-array-using-python/
+
+my_array = [5, 2, 9, 1, 7, 4]
+
+smallest = min(my_array)
+print("Smallest element:", smallest)
+largest = max(my_array)
+print("Largest element:", largest)
