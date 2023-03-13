7a491ac5e4c7d953fb5faa9928bd59e7e17101ba
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-13 15:55:35 +0530

    Create 79.Check whether a character is a vowel or consonant.py

---
+++ 79.Check whether a character is a vowel or consonant.py
@@ -0,0 +1,12 @@
+Link : https://prepinsta.com/python-program/for-checking-a-character-is-a-vowel-or-consonant/
+
+def vowelOrConsonant(x):
+ 
+    if (x == 'a' or x == 'e' or
+        x == 'i' or x == 'o' or x == 'u'):
+        print("Vowel")
+    else:
+        print("Consonant")
+ 
+
+vowelOrConsonant('c')
