4a00feddfee2c1297c0269dd5a737105bf43e471
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-13 21:49:22 +0530

    Create 89.Remove spaces from a string.py

---
+++ 89.Remove spaces from a string.py
@@ -0,0 +1,9 @@
+Link : https://prepinsta.com/python-program/remove-spaces-from-a-string/
+
+
+def remove(string):
+    return string.replace(" ", "")
+     
+
+string = 'a  b c  d '
+print(remove(string))
