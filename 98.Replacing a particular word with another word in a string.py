8fb191c0864b13932043c421dee89297e6f4cd54
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-13 22:57:29 +0530

    Create 98.Replacing a particular word with another word in a string.py

---
+++ 98.Replacing a particular word with another word in a string.py
@@ -0,0 +1,5 @@
+https://prepinsta.com/python-program/to-replace-a-particular-word-by-another-word-from-a-given-string/
+
+string = "The quick brown fox jumps over the lazy dog"
+new_string = string.replace("fox", "cat")
+print(new_string)
