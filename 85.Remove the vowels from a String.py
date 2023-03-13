6d53c3151323969ca7e987ef348a51410d90cfdf
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-13 21:11:26 +0530

    Create 85.Remove the vowels from a String.py

---
+++ 85.Remove the vowels from a String.py
@@ -0,0 +1,13 @@
+Link : https://prepinsta.com/python-program/to-remove-the-vowels-from-a-string/
+
+
+def remove_vowels(string):
+    str = ""
+    vowels = list('aeiouAEIOU')
+    for i in string:
+        if i not in vowels:
+            str = str + i
+    return str
+
+string = 'PrepInsta'
+print(remove_vowels(string))
