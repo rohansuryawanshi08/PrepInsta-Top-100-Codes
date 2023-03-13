db63b3d2a97f738ded32da87b631d47b4bece483
Rohan Suryawanshi <126396108+rohansuryawanshi08@users.noreply.github.com>
2023-03-13 16:05:18 +0530

    Create 80.Check whether a character is a alphabet or not.py

---
+++ 80.Check whether a character is a alphabet or not.py
@@ -0,0 +1,8 @@
+Link : https://prepinsta.com/python-program/to-check-whether-a-character-is-alphabet-or-not/
+
+
+ch = 'a'
+if ch.isalpha():
+    print(ch, "is an alphabet")
+else:
+    print(ch, "is not an alphabet")
